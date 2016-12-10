ee10c307ba845b9e8bdb75e2a6e02a680aba2ef7
Huan Chen <hchen.fred@gmail.com>
2016-12-09 18:05:12 -0800

    Add Unique Path II.js

---
+++ Unique Path II.js
@@ -0,0 +1,60 @@
+/*
+Follow up for "Unique Paths":
+
+Now consider if some obstacles are added to the grids. How many unique paths would there be?
+
+An obstacle and empty space is marked as 1 and 0 respectively in the grid.
+
+For example,
+There is one obstacle in the middle of a 3x3 grid as illustrated below.
+
+[
+  [0,0,0],
+  [0,1,0],
+  [0,0,0]
+]
+The total number of unique paths is 2.
+
+Note: m and n will be at most 100.
+*/
+
+/**
+ * @param {number[][]} obstacleGrid
+ * @return {number}
+ */
+var uniquePathsWithObstacles = function(obstacleGrid) {
+    let m = obstacleGrid.length;
+    let n = obstacleGrid[0].length;
+    let dp = [];
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            dp[i] = new Array(n).fill(0);
+        }
+    }
+
+    for (let i = 0; i < m; i++) {
+        if (obstacleGrid[i][0] === 1) {
+            break;
+        }
+        dp[i][0] = 1;
+    }
+
+    for (let j = 0; j < n; j++) {
+        if (obstacleGrid[0][j] === 1) {
+            break;
+        }
+        dp[0][j] = 1;
+    }
+
+    for (let i = 1; i < m; i++) {
+        for (let j = 1; j < n; j++) {
+            if (obstacleGrid[i][j] === 1) {
+                dp[i][j] = 0;
+            } else {
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+    }
+    return dp[m-1][n-1];
+
+};
